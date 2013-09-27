19cef90f68bd81fbb48f370f0bc73c6948966322
Martin Lazarov <martin@lazarov.bg>
2013-09-27 15:15:55 +0300

    Changing page title

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Haproxy monitor</title>
 	<link href="/css/bootstrap.css" rel="stylesheet">
 	<link href="/css/bootstrap-responsive.css" rel="stylesheet">
 	<link href="/css/custom.css?" rel="stylesheet">
